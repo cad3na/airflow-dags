dae277d46f394f93f01d5b1f1af746e9175ef0e4
Roberto Cadena Vega <roberto@cad3na.com>
2021-07-26 08:24:12 -0500

    Se modifica tamaño de pedazos de csv a procesar

--- covid-analysis.py
+++ covid-analysis.py
@@ -80,7 +80,7 @@ def csv_to_parquet():
     parquet_dir = data_dir/f"{csv_date}.parquet"
 
     if not parquet_dir.exists():
-        chunksize = 20000
+        chunksize = 200000
 
         csv_stream = pd.read_csv(str(csv_file),
                                  dtype=dtypes,
