e45d7db72675fa0a13c4e4e10a7603c05c4ea4fa
Roberto Cadena Vega <roberto@cad3na.com>
2021-08-11 19:25:54 -0500

    Se agrega DAG para actualizar dags :)

---
+++ dag-update.py
@@ -0,0 +1,27 @@
+from datetime import timedelta
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+
+default_args = {
+    'owner': 'roberto',
+    'depends_on_past': False,
+    'start_date': days_ago(2),
+    'email': ['roberto@cad3na.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+dag = DAG(
+    'dag-update',
+    default_args = default_args,
+    catchup = False,
+    schedule_interval = "* */15 * * *",
+)
+
+git_pull = BashOperator(
+    task_id = "update_dags",
+    bash_command = f'cd /home/pi/airflow/dags && git pull',
+    dag = dag,
+)
